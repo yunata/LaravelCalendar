622dd530ea0c0982e1d9d29f1e593d3e56d5ead8
yunata <hanachirusato54@gmail.com>
2018-08-07 16:43:05 +0900

    laravelcalendar

---
+++ laravelCalendar/app/Http/Controllers/EventController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Event;
+use MaddHatter\LaravelFullcalendar\Facades\Calendar;
+
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $events = [];
+        $data = Event::all();
+        if($data->count()) {
+            foreach ($data as $key => $value) {
+                $events[] = Calendar::event(
+                    $value->title,
+                    true,
+                    new \DateTime($value->start_date),
+                    new \DateTime($value->end_date.' +1 day'),
+                    null,
+                    // Add color and link on event
+                    [
+                        'color' => '#f05050',
+                        'url' => 'pass here url and any route',
+                    ]
+                    );
+            }
+        }
+        $calendar = Calendar::addEvents($events);
+        return view('fullcalendar', compact('calendar'));
+
+        }
+}
